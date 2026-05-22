e77af46d1bf339925d33fca594a86256bf05a3a2
追随我心110 <13634195169@139.com>
2026-05-22 17:27:18 +0800

    Delete istudy-rabbitmq directory

--- istudy-rabbitmq/src/main/java/com/istudy/rabbit/Test.java
+++
@@ -1,4 +0,0 @@
-package com.istudy.rabbit;
-
-public class Test {
-}
--- istudy-rabbitmq/src/main/java/com/istudy/rabbit/doc/rabbit/TestRabbit.java
+++
@@ -1,9 +0,0 @@
-package com.istudy.rabbit.doc.rabbit;
-
-/**
- * @Description: TODO
- * @Author Baizhen
- * @Date 2020/3/31 10:16
- */
-public class TestRabbit {
-}
